a625d7859f02ce693eede3b816d1ff65cc133ae7
Jonathan Matheus Ribeiro <jonathanmribeiro@outlook.com>
2023-10-09 10:39:39 -0300

    recursive function

--- details-list/DetailsList/Grid.tsx
+++ details-list/DetailsList/Grid.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { DetailsList, buildColumns, IColumn, IGroup, Image, Link, ImageFit } from '@fluentui/react';
 import { IExampleItem, createListItems } from '@fluentui/example-data';
-import { SortAndGroup, SortItems } from './Utils/SortAndGroup';
+import { IGrouped, SortAndGroup, SortItems } from './Utils/SortAndGroup';
 
 export interface IGridState {
     sortedItems: IExampleItem[];
@@ -17,7 +17,7 @@ export class Grid extends React.Component<IGridProps, IGridState> {
     constructor(props: {}) {
         super(props);
 
-        const items = createListItems(500);
+        const items = createListItems(1000);
 
         this.state = {
             sortedItems: items,
@@ -37,7 +37,7 @@ export class Grid extends React.Component<IGridProps, IGridState> {
             <DetailsList
                 items={sortedItems}
                 columns={columns}
-                onRenderItemColumn={this.renderItemColumn}
+                onRenderItemColumn={this._renderItemColumn}
                 groups={groups}
                 groupProps={{ showEmptyGroups: true }}
             />
@@ -45,7 +45,7 @@ export class Grid extends React.Component<IGridProps, IGridState> {
     }
 
     private _buildColumns(items: IExampleItem[]): IColumn[] {
-        const columns = buildColumns(items, false, this.onColumnClick.bind(this));
+        const columns = buildColumns(items, false, this._onColumnClick.bind(this));
         const thumbnailColumn = columns.filter(column => column.name === 'thumbnail')[0];
 
         thumbnailColumn.name = '';
@@ -62,99 +62,60 @@ export class Grid extends React.Component<IGridProps, IGridState> {
         return columns;
     }
 
+    private _buildGroupLevel(groupLevel: number, parent_group: IGroup, parentGroupedItems: IGrouped, sortColumn: string = '', isSortedDescending: boolean = false): any[] {
+        const { selectedGroups } = this.props;
+        const { columns } = this.state;
+
+        const groupKey = selectedGroups![groupLevel];
+        const nextGroupLevel = groupLevel + 1;
+
+        console.log(`Building group for level ${groupLevel} and will try to build group for next level ${nextGroupLevel}`);
+
+        const mustSortDescending = sortColumn && sortColumn === groupKey ? isSortedDescending : columns.find(column => column.key === groupKey)?.isSortedDescending;
+        parentGroupedItems.children = SortAndGroup(parentGroupedItems.children!, groupKey, mustSortDescending);
+
+        parentGroupedItems.children!.forEach((child, index) => {
+            const startIndex: number = index === 0 ? parentGroupedItems.startIndex : parentGroupedItems.children![index - 1].startIndex + parentGroupedItems.children![index - 1].size;
+            child.startIndex = startIndex;
+
+            const child_group: IGroup = { key: child.key, name: child.key, startIndex: startIndex, count: child.size, level: groupLevel, children: [] };
+
+            if (selectedGroups![nextGroupLevel]) {
+                child.children = this._buildGroupLevel(nextGroupLevel, child_group, child, sortColumn, isSortedDescending);
+            }
+
+            parent_group.children?.push(child_group);
+        });
+
+        return parentGroupedItems.children;
+    }
+
     private _buildGroups(sortColumn: string = '', isSortedDescending: boolean = false) {
         const { columns } = this.state;
-        let { sortedItems, groups } = this.state;
+        let { groups, sortedItems } = this.state;
         const { selectedGroups } = this.props;
 
         if (selectedGroups![0]) {
-            groups = [];
-
+            //const groupedItems = this._buildGroupLevel_0();
             const mustSortDescending = sortColumn && sortColumn === selectedGroups![0] ? isSortedDescending : columns.find(column => column.key === selectedGroups![0])?.isSortedDescending;
             const groupedItems = SortAndGroup(sortedItems, selectedGroups![0], mustSortDescending);
+            groups = [];
 
             groupedItems.forEach((child1, index) => {
                 const startIndex: number = index === 0 ? 0 : groupedItems![index - 1].startIndex + groupedItems![index - 1].size;
                 child1.startIndex = startIndex;
-
-                const child1_group: IGroup = { key: child1.key, name: child1.key, startIndex: startIndex, count: child1.size, level: 1, children: [] };
-
+    
+                const child_group: IGroup = { key: child1.key, name: child1.key, startIndex: startIndex, count: child1.size, level: 0, children: [] };
+    
                 if (selectedGroups![1]) {
-                    const mustSortDescending = sortColumn && sortColumn === selectedGroups![1] ? isSortedDescending : columns.find(column => column.key === selectedGroups![1])?.isSortedDescending;
-                    child1.children = SortAndGroup(child1.children!, selectedGroups![1], mustSortDescending);
-                    child1.children!.forEach((child2, index) => {
-                        const startIndex: number = index === 0 ? child1.startIndex : child1.children![index - 1].startIndex + child1.children![index - 1].size;
-                        child2.startIndex = startIndex;
-
-                        const child2_group: IGroup = { key: child2.key, name: child2.key, startIndex: startIndex, count: child2.size, level: 2, children: [] };
-
-                        if (selectedGroups![2]) {
-                            const mustSortDescending = sortColumn && sortColumn === selectedGroups![2] ? isSortedDescending : columns.find(column => column.key === selectedGroups![2])?.isSortedDescending;
-                            child2.children = SortAndGroup(child2.children!, selectedGroups![2], mustSortDescending);
-                            child2.children!.forEach((child3, index) => {
-                                const startIndex: number = index === 0 ? child2.startIndex : child2.children![index - 1].startIndex + child2.children![index - 1].size;
-                                child3.startIndex = startIndex;
-
-                                const child3_group: IGroup = { key: child3.key, name: child3.key, startIndex: startIndex, count: child3.size, level: 3, children: [] };
-
-                                if (selectedGroups![3]) {
-                                    const mustSortDescending = sortColumn && sortColumn === selectedGroups![3] ? isSortedDescending : columns.find(column => column.key === selectedGroups![3])?.isSortedDescending;
-                                    child3.children = SortAndGroup(child3.children!, selectedGroups![3], mustSortDescending);
-                                    child3.children!.forEach((child4, index) => {
-                                        const startIndex: number = index === 0 ? child3.startIndex : child3.children![index - 1].startIndex + child3.children![index - 1].size;
-                                        child4.startIndex = startIndex;
-
-                                        const child4_group: IGroup = { key: child4.key, name: child4.key, startIndex: startIndex, count: child4.size, level: 4, children: [] };
-
-                                        if (selectedGroups![4]) {
-                                            const mustSortDescending = sortColumn && sortColumn === selectedGroups![4] ? isSortedDescending : columns.find(column => column.key === selectedGroups![4])?.isSortedDescending;
-                                            child4.children = SortAndGroup(child4.children!, selectedGroups![4], mustSortDescending);
-                                            child4.children!.forEach((child5, index) => {
-                                                const startIndex: number = index === 0 ? child4.startIndex : child4.children![index - 1].startIndex + child4.children![index - 1].size;
-                                                child5.startIndex = startIndex;
-
-                                                const child5_group: IGroup = { key: child5.key, name: child5.key, startIndex: startIndex, count: child5.size, level: 5, children: [] };
-
-                                                if (selectedGroups![5]) {
-                                                    const mustSortDescending = sortColumn && sortColumn === selectedGroups![5] ? isSortedDescending : columns.find(column => column.key === selectedGroups![4])?.isSortedDescending;
-                                                    child5.children = SortAndGroup(child5.children!, selectedGroups![5], mustSortDescending);
-                                                }
-
-                                                child5_group.children?.push(child5_group);
-                                            });
-                                        }
-
-                                        child3_group.children!.push(child4_group);
-                                    });
-                                }
-
-                                child2_group.children!.push(child3_group);
-                            });
-                        }
-
-                        child1_group.children!.push(child2_group);
-                    });
+                    child1.children = this._buildGroupLevel(1, child_group, child1, sortColumn, isSortedDescending);
                 }
-
-                groups.push(child1_group);
+    
+                groups.push(child_group);
             });
-
-            let flattenedItems: any[] = [];
-
-            flattenedItems = groupedItems.map(item => item.children).flat();
-
-            if (flattenedItems.find(item => !!item.children))
-                flattenedItems = flattenedItems.map(item => item.children).flat();
-            if (flattenedItems.find(item => !!item.children))
-                flattenedItems = flattenedItems.map(item => item.children).flat();
-            if (flattenedItems.find(item => !!item.children))
-                flattenedItems = flattenedItems.map(item => item.children).flat();
-            if (flattenedItems.find(item => !!item.children))
-                flattenedItems = flattenedItems.map(item => item.children).flat();
-            if (flattenedItems.find(item => !!item.children))
-                flattenedItems = flattenedItems.map(item => item.children).flat();
-
-            sortedItems = flattenedItems;
+            console.log(groupedItems);
+            sortedItems = this._flattenNestedItems(groupedItems);
+            console.log(sortedItems);
         } else {
             const mustSortDescending = sortColumn ? isSortedDescending : columns.find(column => column.key === selectedGroups![0])?.isSortedDescending;
             sortedItems = SortItems(sortedItems, sortColumn, mustSortDescending);
@@ -177,7 +138,26 @@ export class Grid extends React.Component<IGridProps, IGridState> {
             });
     }
 
-    private renderItemColumn(item: IExampleItem, index: number | undefined, column: IColumn | undefined) {
+    private _flattenNestedItems(groupedItems: any[]) {
+        let flattenedItems: any[] = [];
+
+        flattenedItems = groupedItems.map(item => item.children).flat();
+
+        if (flattenedItems.find(item => !!item.children))
+            flattenedItems = flattenedItems.map(item => item.children).flat();
+        if (flattenedItems.find(item => !!item.children))
+            flattenedItems = flattenedItems.map(item => item.children).flat();
+        if (flattenedItems.find(item => !!item.children))
+            flattenedItems = flattenedItems.map(item => item.children).flat();
+        if (flattenedItems.find(item => !!item.children))
+            flattenedItems = flattenedItems.map(item => item.children).flat();
+        if (flattenedItems.find(item => !!item.children))
+            flattenedItems = flattenedItems.map(item => item.children).flat();
+
+        return flattenedItems;
+    }
+
+    private _renderItemColumn(item: IExampleItem, index: number | undefined, column: IColumn | undefined) {
         const fieldContent = item[column?.fieldName as keyof IExampleItem] as string;
 
         switch (column?.key) {
@@ -202,7 +182,7 @@ export class Grid extends React.Component<IGridProps, IGridState> {
         }
     }
 
-    private onColumnClick(ev: React.MouseEvent<HTMLElement>, column: IColumn) {
+    private _onColumnClick(ev: React.MouseEvent<HTMLElement>, column: IColumn) {
         let isSortedDescending = column.isSortedDescending;
 
         if (column.isSorted) {
